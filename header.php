0c70e176b94b367d2a5d979c3705de018c919aba
Noctturne <noctturne17@gmail.com>
2021-09-06 20:06:17 +0200

    Create header and footer basic template

---
+++ header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package BRVO Modding
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <title> Modding Paula </title>
+    </head>
+    <body <?php body_class(); ?>>
+        <div id="page" class="site">
+            <header>
+                <section class="search">Search</section>
+                <section class="top-bar">
+                    <div class="brand"> P </div>
+                    <div class="second-column">
+                        <nav class="main-menu"> Menu </nav>
+                    </div>
+                </section>
+            </header>
--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title> Modding Paula </title>
-    </head>
-    <body>
-        
-    </body>
-</html>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package BRVO Modding
+ */
+?>
+<?php get_header(); ?>
+    <div class="content-area">
+        <main>
+            <section class="hero"> Hero Section </section>
+            <section class="custom"> Custom Section </section>
+            <section class="popular-products"> Popular Products Section </section>
+            <section class="new-arrival"> New Arrival Section </section>
+        </main>
+    </div>
+<?php get_footer(); ?>
