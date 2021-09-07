27897439bd53b081ef607d2968f5e88ce21cdbd4
Noctturne <noctturne17@gmail.com>
2021-09-07 16:48:30 +0200

    Create page template for cart, checkout and my account

--- page.php
+++ page.php
@@ -1,11 +1,7 @@
 <?php
 /**
- * The main template file
+ * Page template file
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -13,7 +9,6 @@
  */
 ?>
 <?php get_header(); ?>
-    <section class="products"> 
         <div class="container">
             <div class="row">
                 <?php 
@@ -32,5 +27,4 @@
                     <?php endif; ?>
             </div>
         </div>
-    </section>
 <?php get_footer(); ?>
