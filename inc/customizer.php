ddee2e4ab077966e416edd341425e657d0646461
Noctturne <noctturne17@gmail.com>
2021-09-08 11:40:51 +0200

    Customize Popular products and New Arrivals sections

--- inc/customizer.php
+++ inc/customizer.php
@@ -6,30 +6,152 @@
  */
 
  function brvo_modding_customizer( $wp_customize ){
-    // Copyright section
-    $wp_customize->add_section(
-        'section_copyright', array(
-            'title' => 'Copyright Settings',
-            'description' => 'Copyright Section'
-        )
-    );
+/*          --------- HOME PAGE ---------           */
 
-    // Field - Copyright Text Box
-    $wp_customize->add_setting(
-        'set_copyright', array(
-            'type' => 'theme_mod',
-            'default' => '',
-            'sanitize_callback' => 'sanitize_text_field'
-        )
-    );
 
-    $wp_customize->add_control(
-        'set_copyright', array(
-            'label' => 'Copyright',
-            'description' => 'Add your copyright information here',
-            'section' => 'section_copyright',
-            'type' => 'text'
-        )
-    );
+        $wp_customize->add_section(
+            'section_home_page', array( 
+                'title' => 'Home Page Products Settings',
+                'description' => 'Home Page Section'
+            )
+        );
+        /* ----- POPULAR PRODUCTS ----- */
+            // Popular products title
+            $wp_customize->add_setting(
+                'set_popular_title', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'sanitize_text_field'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_popular_title', array( 
+                    'label' => 'Popular products Title',
+                    'description' => 'Popular Products Title',
+                    'section' => 'section_home_page',
+                    'type' => 'text'
+                )
+            );
+
+            // Popular products Limit
+            $wp_customize->add_setting(
+                'set_popular_max_num', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'absint'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_popular_max_num', array( 
+                    'label' => 'Popular Products Max Number',
+                    'description' => 'Popular products max number',
+                    'section' => 'section_home_page',
+                    'type' => 'number'
+                )
+            );
+
+            // Popular products Columns
+            $wp_customize->add_setting(
+                'set_popular_max_col', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'absint'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_popular_max_col', array( 
+                    'label' => 'Popular Products Max Columns',
+                    'description' => 'Popular products max columns',
+                    'section' => 'section_home_page',
+                    'type' => 'number'
+                )
+            );
+
+        /* ----- NEW ARRIVALS ----- */
+            // New arrivals title
+            $wp_customize->add_setting(
+                'set_new_arrivals_title', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'sanitize_text_field'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_new_arrivals_title', array( 
+                    'label' => 'New Arrivals Title',
+                    'description' => 'New Arrivals Title',
+                    'section' => 'section_home_page',
+                    'type' => 'text'
+                )
+            );
+
+            // New arrivals Limit
+            $wp_customize->add_setting(
+                'set_new_arrivals_max_num', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'absint'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_new_arrivals_max_num', array( 
+                    'label' => 'New Arrivals Max Number',
+                    'description' => 'New Arrivals max number',
+                    'section' => 'section_home_page',
+                    'type' => 'number'
+                )
+            );
+
+            // New Arrivals Columns
+            $wp_customize->add_setting(
+                'set_new_arrivals_max_col', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'absint'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_new_arrivals_max_col', array( 
+                    'label' => 'New Arrivals Max Columns',
+                    'description' => 'New Arrivals max columns',
+                    'section' => 'section_home_page',
+                    'type' => 'number'
+                )
+            );
+
+
+
+/*          --------- FOOTER ---------           */      
+        // Copyright section
+        $wp_customize->add_section(
+            'section_copyright', array(
+                'title' => 'Copyright Settings',
+                'description' => 'Copyright Section'
+            )
+        );
+
+            // Field - Copyright Text Box
+            $wp_customize->add_setting(
+                'set_copyright', array(
+                    'type' => 'theme_mod',
+                    'default' => '',
+                    'sanitize_callback' => 'sanitize_text_field'
+                )
+            );
+
+            $wp_customize->add_control(
+                'set_copyright', array(
+                    'label' => 'Copyright',
+                    'description' => 'Add your copyright information here',
+                    'section' => 'section_copyright',
+                    'type' => 'text'
+                )
+            );
  }
  add_action( 'customize_register', 'brvo_modding_customizer' );
--- template-home.php
+++ template-home.php
@@ -13,22 +13,41 @@
                 Custom Section
             </div>
         </section>
+
+        <?php
+        /* Only show these section if WooCommerce is active */ 
+        if( class_exists ( 'WooCommerce' )):
+        ?>
         <section class="popular-products"> 
+            <?php 
+
+            /* DATA FROM CUSTOMIZE */
+            $popular_limit = get_theme_mod( 'set_popular_max_num', 4);
+            $popular_col = get_theme_mod( 'set_popular_max_col', 4);
+            $new_arrivals_limit = get_theme_mod( 'set_new_arrivals_max_num', 3);
+            $new_arrivals_col = get_theme_mod( 'set_new_arrivals_max_col', 3);
+
+            ?>
             <div class="container">
                 <div class="row">
-                    <h1 class="text-center"> Popular Products </h1>
-                    <?php echo do_shortcode('[products limit="4" columns="4" orderby="popularity"]'); ?>
+                    <div class="section-title">
+                        <h1 class="text-center"> <?php echo get_theme_mod( 'set_popular_title', 'Popular Products') ?> </h1>
+                    </div>
+                   <?php echo do_shortcode( '[products limit=" ' . $popular_limit . ' " columns=" ' . $popular_col . ' " orderby="popularity"]' ); ?>
                 </div>
             </div>
         </section>
         <section class="new-arrival"> 
             <div class="container">
                 <div class="row">
-                    <h1 class="text-center"> New Arrival </h1>
-                    <?php echo do_shortcode('[products limit="3" columns="3" orderby="date"]'); ?>
+                    <div class="section-title">
+                        <h1 class="text-center"> <?php echo get_theme_mod( 'set_new_arrivals_title', 'New Arrivals') ?> </h1>
+                    </div>
+                    <?php echo do_shortcode( '[products limit=" ' . $new_arrivals_limit . ' " columns=" ' . $new_arrivals_col . ' " orderby="date"]' ); ?>
                 </div>
             </div>
         </section>
     </main>
-
+    <?php endif; ?>
+	<!-- End class_exists for WooCommerce -->
 <?php get_footer(); ?>
