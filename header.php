736a5f084ba8dbcb71535574a29d2b3844de4e63
Noctturne <noctturne17@gmail.com>
2021-09-08 17:07:33 +0200

    Finish test :)

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <title> Modding </title>
+        <link rel="profile" href="https://gmpg.org/xfn/11" />
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
